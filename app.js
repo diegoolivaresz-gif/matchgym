3a6c1caac7845eb2674f3b2b1027c8f32b1608d5
diegoolivaresz-gif <diego.olivares.z@mail.pucv.cl>
2025-11-27 04:22:18 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCC5XJy3oqmDoTKgDMefB1zjp7XAFgTxWo",
+    authDomain: "matchgym-e7df0.firebaseapp.com",
+    projectId: "matchgym-e7df0",
+    storageBucket: "matchgym-e7df0.firebasestorage.app",
+    messagingSenderId: "766710296254",
+    appId: "1:766710296254:web:a01ca1d2ee5d362b45b8ef",
+    measurementId: "G-D4VWL0H0K9"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// resto del código...
